fd845f246a2f4059ff96889da7fdae007aea64b4
AbhishekDeveleper <abhisheksingh55568@gmail.com>
2023-06-08 12:12:45 +0530

    next.config changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/next-js01",
-    experimental: {
-        appDir: true,
-    },
 };
 
 module.exports = nextConfig;
