1a1c7e18ceb9193855ebe11340d992c243625e4d
AbhishekDeveleper <abhisheksingh55568@gmail.com>
2023-06-28 12:00:39 +0530

    changes small

---
+++ src/app/error.js
@@ -0,0 +1,48 @@
+// <Link href="#dealers_sec">
+//               <svg className="header_icons">
+//                 <use xlinkHref="sprite.svg#icon-user" />
+//               </svg>
+//             </Link>
+//             <Link href="#dealers_sec" className="sidebarlink">
+//               Dealers
+//             </Link>
+//           </div>
+//           <div className="containers">
+//             <Link href="#features_sec">
+//               <svg className="header_icons">
+//                 <use xlinkHref="sprite.svg#icon-open-book" />
+//               </svg>
+//             </Link>
+//             <Link href="#features_sec" className="sidebarlink">
+//               Feature
+//             </Link>
+//           </div>
+//           <div className="containers">
+//             <Link href="#about_sec">
+//               <svg className="header_icons">
+//                 <use xlinkHref="sprite.svg#icon-book" />
+//               </svg>
+//             </Link>
+//             <Link href="#about_sec" className="sidebarlink">
+//               About
+//             </Link>
+//           </div>
+//           <div className="containers">
+//             <Link href="#booking_sec">
+//               <svg className="header_icons">
+//                 <use xlinkHref="sprite.svg#icon-home1" />
+//               </svg>
+//             </Link>
+//             <Link href="#booking_sec" className="sidebarlink">
+//               Booking
+//             </Link>
+//           </div>
+//           <div className="containers">
+//             <Link href="#homes_sec">
+//               <svg className="header_icons">
+//                 <use xlinkHref="sprite.svg#icon-image-inverted" />
+//               </svg>
+//             </Link>
+//             <Link href="#homes_sec" className="sidebarlink">
+//               Gallery
+//             </Link>
