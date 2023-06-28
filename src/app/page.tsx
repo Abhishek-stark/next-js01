7c043e1ec20a5897aa78cd2aa70aea03d3658173
AbhishekDeveleper <abhisheksingh55568@gmail.com>
2023-06-28 11:58:04 +0530

    some small bugs removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,56 +17,7 @@ export default function Home() {
           id={`${shrink ? "closedcontainer" : "opencontainer"}`}
         >
           <button className="sidebar_btn" onClick={menubtn}></button>
-          <div className="containers">
-            <Link href="#dealers_sec">
-              <svg className="header_icons">
-                <use xlinkHref="sprite.svg#icon-user" />
-              </svg>
-            </Link>
-            <Link href="#dealers_sec" className="sidebarlink">
-              Dealers
-            </Link>
-          </div>
-          <div className="containers">
-            <Link href="#features_sec">
-              <svg className="header_icons">
-                <use xlinkHref="sprite.svg#icon-open-book" />
-              </svg>
-            </Link>
-            <Link href="#features_sec" className="sidebarlink">
-              Feature
-            </Link>
-          </div>
-          <div className="containers">
-            <Link href="#about_sec">
-              <svg className="header_icons">
-                <use xlinkHref="sprite.svg#icon-book" />
-              </svg>
-            </Link>
-            <Link href="#about_sec" className="sidebarlink">
-              About
-            </Link>
-          </div>
-          <div className="containers">
-            <Link href="#booking_sec">
-              <svg className="header_icons">
-                <use xlinkHref="sprite.svg#icon-home1" />
-              </svg>
-            </Link>
-            <Link href="#booking_sec" className="sidebarlink">
-              Booking
-            </Link>
-          </div>
-          <div className="containers">
-            <Link href="#homes_sec">
-              <svg className="header_icons">
-                <use xlinkHref="sprite.svg#icon-image-inverted" />
-              </svg>
-            </Link>
-            <Link href="#homes_sec" className="sidebarlink">
-              Gallery
-            </Link>
-          </div>
+          <div className="containers"></div>
         </div>
       </div>
       <div className="header" id="dealers_sec">
